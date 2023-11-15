31a655aa908cdeeccb9f41236224ad0a0845b88b
soeun135 <soensuckun@naver.com>
2023-11-16 02:51:43 +0900

    Initial commit

---
+++ src/main/java/com/soeun/GiftFunding/GiftFundingApplication.java
@@ -0,0 +1,13 @@
+package com.soeun.GiftFunding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiftFundingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiftFundingApplication.class, args);
+	}
+
+}
