02e06d533c1f7e9c1a9ad4dcc96fcd469bd06b6d
soeun135 <soensuckun@naver.com>
2023-11-22 23:07:07 +0900

    test : 회원가입 기능 테스트

---
+++ src/test/java/com/soeun/GiftFunding/service/UserServiceTest.java
@@ -0,0 +1,72 @@
+package com.soeun.GiftFunding.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+import com.soeun.GiftFunding.dto.Signup;
+import com.soeun.GiftFunding.dto.Signup.Request;
+import com.soeun.GiftFunding.dto.Signup.Response;
+import com.soeun.GiftFunding.entity.User;
+import com.soeun.GiftFunding.repository.UserRepository;
+import com.soeun.GiftFunding.type.Authority;
+import java.time.LocalDate;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.crypto.bcrypt.BCrypt;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@ExtendWith(MockitoExtension.class)
+class UserServiceTest {
+    @Mock
+    private UserRepository userRepository;
+
+    @Mock
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @InjectMocks
+    private UserServiceImpl userServiceimpl;
+
+    @Test
+    void successSignup() {
+        //given
+        Request request = Request.builder()
+            .userName("soni")
+            .phone("01011111111")
+            .email("test@naver.com")
+            .password("1111") //인코딩해서 넣어야함
+            .address("경기도")
+            .birthDay("2000-01-28")
+            .build();
+
+        given(userRepository.save(any()))
+            .willReturn(User
+                .builder()
+                .userName("soni")
+                .phone("01011111111")
+                .email("test@naver.com")
+                .password("1111") //인코딩해서 넣어야함
+                .address("경기도")
+                .birthDay(LocalDate.parse("2000-01-28"))
+                .role(Authority.ROLE_USER)
+                .build());
+        //when
+        ArgumentCaptor<User> captor =
+            ArgumentCaptor.forClass(User.class);
+
+        Response response = userServiceimpl.singUp(request);
+
+        //then
+        verify(userRepository, times(1))
+            .save(captor.capture());
+        assertEquals(request.getUserName(),response.getUserName());
+        assertEquals(request.getUserName(),captor.getValue().getUserName());
+
+     }
+}
