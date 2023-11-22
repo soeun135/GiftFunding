1d72d9dd296bbddd698f4559f6d5c7d018609ee1
soeun135 <soensuckun@naver.com>
2023-11-22 23:04:30 +0900

    test : 회원가입 테스트

---
+++ src/test/java/com/soeun/GiftFunding/GiftFundingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soeun.GiftFunding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiftFundingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
