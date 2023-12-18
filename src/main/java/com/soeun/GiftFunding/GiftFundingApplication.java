cbe8d14a5e4142b9c3edb00a00339a166a1cf36d
soeun135 <soensuckun@naver.com>
2023-12-18 17:54:49 +0900

    refactor : 컨트롤러 경로 입력시 html 파일 뜨도록 수정

--- src/main/java/com/soeun/GiftFunding/GiftFundingApplication.java
+++ src/main/java/com/soeun/GiftFunding/GiftFundingApplication.java
@@ -2,8 +2,9 @@ package com.soeun.GiftFunding;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
-//@EnableScheduling
+@EnableScheduling
 @SpringBootApplication
 public class GiftFundingApplication {
 
--- src/main/java/com/soeun/GiftFunding/controller/PaymentController.java
+++ src/main/java/com/soeun/GiftFunding/controller/PaymentController.java
@@ -4,6 +4,7 @@ import com.soeun.GiftFunding.service.PaymentService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,9 +12,15 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 @Slf4j
 @RequiredArgsConstructor
+@RequestMapping("/pay")
 public class PaymentController {
     private final PaymentService paymentService;
-    @RequestMapping(value = "/pay/success", method = RequestMethod.POST)
+
+    @GetMapping
+    public String payRequest() {
+        return "/payment.html";
+    }
+    @RequestMapping(value = "/success", method = RequestMethod.POST)
     public void paySuccess(@RequestParam("payAmount") long amount,
         @RequestParam("memberId") long memberId) {
 
