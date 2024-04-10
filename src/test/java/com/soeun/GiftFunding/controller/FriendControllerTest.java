574bf971ef6731d0026bdfe06aee144c0a7429f4
soeun135 <soensuckun@naver.com>
2024-04-10 21:11:12 +0900

    [test #26] friendRequet API 테스트 성공

--- src/test/java/com/soeun/GiftFunding/controller/FriendControllerTest.java
+++ src/test/java/com/soeun/GiftFunding/controller/FriendControllerTest.java
@@ -1,5 +1,8 @@
 package com.soeun.GiftFunding.controller;
 
+import com.epages.restdocs.apispec.MockMvcRestDocumentationWrapper;
+import com.epages.restdocs.apispec.ResourceSnippetParameters;
+import com.epages.restdocs.apispec.Schema;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.soeun.GiftFunding.config.SecurityConfig;
 import com.soeun.GiftFunding.dto.*;
@@ -11,6 +14,7 @@ import com.soeun.GiftFunding.type.FriendState;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.ComponentScan;
@@ -20,18 +24,24 @@ import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.MediaType;
+import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
+import org.springframework.restdocs.payload.JsonFieldType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.List;
 
+import static com.epages.restdocs.apispec.MockMvcRestDocumentationWrapper.document;
 import static com.soeun.GiftFunding.type.FriendState.ACCEPT;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.when;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -42,6 +52,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
                 @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = JwtAuthenticationFilter.class)
         }
 )
+@AutoConfigureRestDocs
 class FriendControllerTest {
     @MockBean
     private FriendServiceImpl friendService;
@@ -74,7 +85,8 @@ class FriendControllerTest {
                         .build());
         //when
         //then
-        mockMvc.perform(post("/friend/request")
+        mockMvc.perform(RestDocumentationRequestBuilders
+                        .post("/friend/request")
                         .header("Authorization", "Bearer AccessToken")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(request))
@@ -82,7 +94,24 @@ class FriendControllerTest {
                 ).andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.email").value(request.getEmail()))
-                .andExpect(jsonPath("$.message").value("님에게 친구요청을 보냈습니다."));
+                .andExpect(jsonPath("$.message").value("님에게 친구요청을 보냈습니다."))
+                .andDo(document("/friend/request",
+                                ResourceSnippetParameters.builder()
+                                        .tag("friend")
+                                        .summary("friend request API")
+                                        .description("친구 요청")
+
+                                        .requestSchema(Schema.schema("FriendRequest.Request"))
+                                        .responseSchema(Schema.schema("FriendRequest.Response"))
+                                , preprocessRequest(prettyPrint())
+                                , preprocessResponse(prettyPrint())
+                                , requestFields(
+                                        fieldWithPath("email").type(JsonFieldType.STRING).description("친구 요청 걸 상대방 이메일"))
+                                , responseFields(
+                                        fieldWithPath("email").type(JsonFieldType.STRING).description("친구 요청 걸 상대방 이메일"),
+                                        fieldWithPath("message").type(JsonFieldType.STRING).description("메세지"))
+                        )
+                );
     }
 
     @Test
@@ -112,7 +141,7 @@ class FriendControllerTest {
 
         //when
         //then
-        mockMvc.perform(get("/friend")
+        mockMvc.perform(RestDocumentationRequestBuilders.get("/friend")
                         .header("Authorization", "Bearer AccessToken")
                         .param("page", "0")
                         .param("size", "10")
@@ -122,7 +151,12 @@ class FriendControllerTest {
                 .andExpect(jsonPath("$.content.[0].memberName").value("버니"))
                 .andExpect(jsonPath("$.content.[0].memberEmail").value("buni@naver.com"))
                 .andExpect(jsonPath("$.content.[1].memberName").value("벅스"))
-                .andExpect(jsonPath("$.content.[1].memberEmail").value("bucks@naver.com"));
+                .andExpect(jsonPath("$.content.[1].memberEmail").value("bucks@naver.com"))
+                .andDo(MockMvcRestDocumentationWrapper.document("/friend/List",
+                        ResourceSnippetParameters.builder()
+                                .tag("friend")
+                                .summary("friend List API")
+                                .description("친구 목록 조회 ")));
     }
 
     @Test
@@ -196,7 +230,8 @@ class FriendControllerTest {
                 .andExpect(jsonPath("$.fundingProductList.content.[0].product.productName").value("반지"))
                 .andExpect(jsonPath("$.fundingProductList.content.[1].product.productName").value("목걸이"));
 
-     }
+    }
+
     @Test
     @DisplayName("친구 요청 동시성 테스트")
     void redissonLockTest() {
