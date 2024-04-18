c979b393a81ca13b9d59e72d2bac4562bef88fa5
soeun135 <soensuckun@naver.com>
2024-04-19 03:34:27 +0900

    [test #25] MemberController reissue , userInfo API test clear

--- src/test/java/com/soeun/GiftFunding/domain/member/controller/MemberControllerTest.java
+++ src/test/java/com/soeun/GiftFunding/domain/member/controller/MemberControllerTest.java
@@ -1,14 +1,20 @@
 package com.soeun.GiftFunding.domain.member.controller;
 
-import com.epages.restdocs.apispec.MockMvcRestDocumentationWrapper;
 import com.epages.restdocs.apispec.ResourceSnippetParameters;
 import com.epages.restdocs.apispec.Schema;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.soeun.GiftFunding.config.SecurityConfig;
 import com.soeun.GiftFunding.domain.member.dto.Signin;
 import com.soeun.GiftFunding.domain.member.dto.Signup;
+import com.soeun.GiftFunding.domain.member.dto.UserInfoResponse;
 import com.soeun.GiftFunding.domain.member.service.MemberServiceImpl;
+import com.soeun.GiftFunding.domain.product.entity.Product;
+import com.soeun.GiftFunding.dto.FundingProductDto;
+import com.soeun.GiftFunding.dto.MemberAdapter;
+import com.soeun.GiftFunding.dto.ReissueResponse;
+import com.soeun.GiftFunding.mock.WithMockUser;
 import com.soeun.GiftFunding.security.JwtAuthenticationFilter;
+import com.soeun.GiftFunding.type.FundingState;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,8 +29,17 @@ import org.springframework.restdocs.payload.JsonFieldType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.context.WebApplicationContext;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static com.epages.restdocs.apispec.MockMvcRestDocumentationWrapper.document;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
+import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
+import static org.springframework.restdocs.headers.HeaderDocumentation.requestHeaders;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
 import static org.springframework.restdocs.payload.PayloadDocumentation.*;
@@ -53,6 +68,11 @@ class MemberControllerTest {
     @Autowired
     private ObjectMapper objectMapper;
 
+    private MemberAdapter memberAdapter1() {
+        return new MemberAdapter("soni@naver.com", "qwerty");
+    }
+
+    @WithMockUser
     @Test
     @DisplayName("회원가입 성공 테스트")
     void singUpSuccessTest() throws Exception {
@@ -78,7 +98,7 @@ class MemberControllerTest {
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.name").value("소은"))
-                .andDo(MockMvcRestDocumentationWrapper.document("/member/signup",
+                .andDo(document("/member/signup",
                         ResourceSnippetParameters.builder()
                                 .tag("member")
                                 .summary("signup API")
@@ -101,6 +121,7 @@ class MemberControllerTest {
                         )));
     }
 
+    @WithMockUser
     @Test
     @DisplayName("로그인 성공 테스트")
     void singInSuccessTest() throws Exception {
@@ -121,13 +142,15 @@ class MemberControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.accessToken").value("AccessToken"))
                 .andExpect(jsonPath("$.refreshToken").value("RefreshToken"))
-                .andDo(MockMvcRestDocumentationWrapper.document("/member/signin",
+                .andDo(document("/member/signin",
                                 ResourceSnippetParameters.builder()
                                         .tag("member")
                                         .summary("signin API")
                                         .description("로그인")
                                         .requestSchema(Schema.schema("Signin.Request"))
                                         .responseSchema(Schema.schema("Signin.Response"))
+                                , preprocessRequest(prettyPrint())
+                                , preprocessResponse(prettyPrint())
                                 , requestFields(
                                         fieldWithPath("email").type(JsonFieldType.STRING).description("이메일")
                                         , fieldWithPath("password").type(JsonFieldType.STRING).description("비밀번호")
@@ -138,5 +161,117 @@ class MemberControllerTest {
                                 )
                         )
                 );
+    }
+
+    @Test
+    @DisplayName("AccessToken 재발급 성공 테스트")
+    @WithMockUser
+    void reissueSuccessTest() throws Exception {
+        //given
+        String Authorization = "RefreshToken for Reissue AccessToken";
+        given(memberService.reissue(Authorization))
+                .willReturn(new ReissueResponse(
+                        "AccessToken",
+                        "RefreshToken"));
+        //when
+        //then
+        mockMvc.perform(post("/member/reissue")
+                        .header("Authorization", Authorization))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.accessToken").value("AccessToken"))
+                .andExpect(jsonPath("$.refreshToken").value("RefreshToken"))
+                .andDo(document("/member/reissue",
+                        ResourceSnippetParameters.builder()
+                                .tag("member")
+                                .summary("reissue API")
+                                .description("토큰 재발급")
+                                .responseSchema(Schema.schema("ReissueResponse"))
+                        , preprocessRequest(prettyPrint())
+                        , preprocessResponse(prettyPrint())
+                        , requestHeaders(
+                                headerWithName("Authorization").description("Refresh Token"))
+                        , responseFields(
+                                fieldWithPath("accessToken").type(JsonFieldType.STRING).description("AccessToken")
+                                , fieldWithPath("refreshToken").type(JsonFieldType.STRING).description("RefreshToken")))
+                );
+    }
+
+    @Test
+    @DisplayName("본인 정보 조회 성공 테스트")
+    void userInfoSuccessTest() throws Exception {
+        //given
+        Product product1 = new Product(1L, "반지", 10000L, 1);
+        Product product2 = new Product(2L, "목걸이", 20000L, 2);
+
+        List fundingProductDtoList = Arrays.asList(
+                FundingProductDto.builder()
+                        .id(1L)
+                        .product(product1)
+                        .total(5000L)
+                        .createdAt(LocalDate.now())
+                        .expiredAt(LocalDate.now().plusYears(1))
+                        .fundingState(FundingState.ONGOING)
+                        .build(),
+                FundingProductDto.builder()
+                        .id(2L)
+                        .product(product2)
+                        .total(1000L)
+                        .createdAt(LocalDate.now())
+                        .expiredAt(LocalDate.now().plusYears(1))
+                        .fundingState(FundingState.ONGOING)
+                        .build()
+        );
+
+        given(memberService.userInfo(any()))
+                .willReturn(new UserInfoResponse(
+                        "소은",
+                        "010-1111-1111",
+                        "soni@naver.com",
+                        "서울특별시 강남구",
+                        LocalDate.of(2000, 01, 28),
+                        new ArrayList<>(fundingProductDtoList)
+                ));
+        //when
+        //then
+        mockMvc.perform(get("/member/info")
+                        .header("Authorization", "Bearer AccessToken"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name").value("소은"))
+                .andExpect(jsonPath("$.phone").value("010-1111-1111"))
+                .andExpect(jsonPath("$.email").value("soni@naver.com"))
+                .andExpect(jsonPath("$.address").value("서울특별시 강남구"))
+                .andExpect(jsonPath("$.birthDay").value("2000-01-28"))
+                .andExpect(jsonPath("$.fundingProductList.[0].id").value(1L))
+                .andExpect(jsonPath("$.fundingProductList.[0].product.productName").value("반지"))
+                .andExpect(jsonPath("$.fundingProductList.[1].id").value(2L))
+                .andExpect(jsonPath("$.fundingProductList.[1].product.productName").value("목걸이"))
+                .andDo(document("/member/info",
+                        ResourceSnippetParameters.builder()
+                                .tag("member")
+                                .summary("user info API")
+                                .description("로그인 한 사용자 정보 조회")
+                                .responseSchema(Schema.schema("UserInfoResponse"))
+                                .responseFields(
+                                        fieldWithPath("name").type(JsonFieldType.STRING).description("이름")
+                                        , fieldWithPath("phone").type(JsonFieldType.STRING).description("전화번호")
+                                        , fieldWithPath("email").type(JsonFieldType.STRING).description("이메일")
+                                        , fieldWithPath("address").type(JsonFieldType.STRING).description("주소")
+                                        , fieldWithPath("birthDay").type(JsonFieldType.STRING).description("생일")
+                                        , fieldWithPath("fundingProductList[].id").type(JsonFieldType.NUMBER).description("펀딩상품 리스트 아이디")
+                                        , fieldWithPath("fundingProductList[].product.id").type(JsonFieldType.NUMBER).description("상품 아이디")
+                                        , fieldWithPath("fundingProductList[].product.productName").type(JsonFieldType.STRING).description("상품 이름")
+                                        , fieldWithPath("fundingProductList[].product.price").type(JsonFieldType.NUMBER).description("상품 가격")
+                                        , fieldWithPath("fundingProductList[].product.ranking").type(JsonFieldType.NUMBER).description("상품 순위")
+                                        , fieldWithPath("fundingProductList[].total").type(JsonFieldType.NUMBER).description("펀딩 총 금액")
+                                        , fieldWithPath("fundingProductList[].createdAt").type(JsonFieldType.STRING).description("펀딩 시작일")
+                                        , fieldWithPath("fundingProductList[].expiredAt").type(JsonFieldType.STRING).description("펀딩 종료일")
+                                        , fieldWithPath("fundingProductList[].fundingState").type(JsonFieldType.STRING).description("펀딩 상태"))
+                        , requestHeaders(
+                                headerWithName("Authorization").description("4eeBearer AccessToken")
+                        )
+                ));
+
     }
 }
