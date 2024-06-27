8c296d41baea59876c5c9f1de7925e6bef59c04e
soeun135 <soensuckun@naver.com>
2024-06-27 20:20:23 +0900

    [test #32] jar 실행 시 출력 보도록 코드 임시 수정

--- deploy.sh
+++ deploy.sh
@@ -30,4 +30,5 @@ else
 fi
 
 echo "> $APPLICATION_JAR_NAME 배포"
-nohup java -jar $APPLICATION_JAR > /dev/null 2> /dev/null < /dev/null &
+nohup java -jar $APPLICATION_JAR
+#nohup java -jar $APPLICATION_JAR > /dev/null 2> /dev/null < /dev/null &
