a247fac2dee39c396e7a559168228f7b7d47f178
soeun135 <soensuckun@naver.com>
2024-06-29 00:09:49 +0900

    [release #32] deploy.sh에서 java 실행 시 --spring.config.location 제거, CD_WorkFlow에서 체크 아웃 시 서브 모듈 사용하도록 옵션 추가

--- deploy.sh
+++ deploy.sh
@@ -31,5 +31,3 @@ fi
 
 echo "> $APPLICATION_JAR_NAME 배포"
 nohup java -jar $APPLICATION_JAR > /dev/null 2> /dev/null < /dev/null &
-#\
-#  --spring.config.location=classpath:/config/application-prod.yml \
